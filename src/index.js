e6e4350783af14dc53a6d3205f4775751712f1f2
Eric Kisee <erickiseemulwa@gmail.com>
2021-06-25 16:51:26 +0300

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './jsx.js';
 
-ReactDOM.render(
-    <h1>Hello, World</h1>,
+// ReactDOM.render(
+//     <h1>Hello, World</h1>,
+//     document.getElementById('root')
+// );
+
+const name = 'Perez';
+const _element = <h1>Hello {name}</h1>
+
+function formatName (user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Eric',
+    lastName: 'Kisee'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+ReactDOM.render (
+    element,
     document.getElementById('root')
 );

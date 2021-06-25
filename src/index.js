3828b80e7a155f5287d9585ae6e543ba750b074b
Eric Kisee <erickiseemulwa@gmail.com>
2021-06-25 17:04:25 +0300

    Rendering Elements

--- src/index.js
+++ src/index.js
@@ -7,25 +7,37 @@ import './index.css';
 //     document.getElementById('root')
 // );
 
-const name = 'Perez';
-const _element = <h1>Hello {name}</h1>
+// const name = 'Perez';
+// const _element = <h1>Hello {name}</h1>
 
-function formatName (user) {
-    return user.firstName + ' ' + user.lastName;
+// function formatName (user) {
+//     return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//     firstName: 'Eric',
+//     lastName: 'Kisee'
+// };
+
+// const element = (
+//     <h1>
+//         Hello, {formatName(user)}!
+//     </h1>
+// );
+
+// ReactDOM.render (
+//     element,
+//     document.getElementById('root')
+// );
+
+function tick () {
+    const element = (
+        <div>
+            <h1>Hello World</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
 }
 
-const user = {
-    firstName: 'Eric',
-    lastName: 'Kisee'
-};
-
-const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
-);
-
-ReactDOM.render (
-    element,
-    document.getElementById('root')
-);
+setInterval(tick, 1000);

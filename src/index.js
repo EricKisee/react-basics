4029a353e763599b1b6f4dabf366f2af01f025f6
Eric Kisee <erickiseemulwa@gmail.com>
2021-06-28 17:36:52 +0300

    Forms.1

--- src/index.js
+++ src/index.js
@@ -341,21 +341,58 @@ import './index.css';
 //     document.getElementById('root')
 // );
 
-function ListItem ( props ) {
-    return <li>{props.value}</li> ;
+// function ListItem ( props ) {
+//     return <li>{props.value}</li> ;
+// }
+
+// function NumberList (props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map ((number) => 
+//         <ListItem key={number.toString()} value={number}/>
+//     );
+//     return (
+//         <ul>{listItems}</ul>
+//     );
+// }
+// const numbers = [1,2,3,4,5];
+// ReactDOM.render(
+//     <NumberList numbers = {numbers} />,
+//     document.getElementById('root')
+// );
+
+class NameForm extends React.Component {
+
+    constructor (props) {
+        super (props);
+        this.state = { value : ''};
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange (event) {
+        this.setState({value : event.target.value});
+    }
+
+    handleSubmit (event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault ();
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="submit" />
+            </form>
+        );
+    }
 }
 
-function NumberList (props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map ((number) => 
-        <ListItem key={number.toString()} value={number}/>
-    );
-    return (
-        <ul>{listItems}</ul>
-    );
-}
-const numbers = [1,2,3,4,5];
-ReactDOM.render(
-    <NumberList numbers = {numbers} />,
+ReactDOM.render (
+    <NameForm/>,
     document.getElementById('root')
 );

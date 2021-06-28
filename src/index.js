060f28fe0e46d64cb41b414db10ce199990a014c
Eric Kisee <erickiseemulwa@gmail.com>
2021-06-28 16:57:39 +0300

    Lists and Keys

--- src/index.js
+++ src/index.js
@@ -274,44 +274,88 @@ import './index.css';
 //   }
 
 
-function WarningBanner (props) {
-    if (!props.warn) {
-        return null;
-    }
+// function WarningBanner (props) {
+//     if (!props.warn) {
+//         return null;
+//     }
 
+//     return (
+//         <div className="warning">
+//             Warning!
+//         </div>
+//     );
+// }
+
+// class Page extends React.Component {
+
+//     constructor (props) {
+//         super (props);
+//         this.state = {showWarning : true};
+//         this.handleToggleClick = this.handleToggleClick.bind(this);
+//     }
+
+//     handleToggleClick () {
+//         this.setState (state => ({
+//             showWarning : !state.showWarning
+//         }));
+//     }
+
+//     render () {
+//         return ( 
+//             <div>
+//                 <WarningBanner warn = {this.state.showWarning} />
+//                 <button onClick = {this.handleToggleClick}>
+//                     {this.state.showWarning ? 'hide' : 'show'}
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render (
+//     <Page/>,
+//     document.getElementById('root')
+// );
+
+// const numbers = [1,2,3,4,5];
+// const listItems = numbers.map ((number) => 
+//     <li>{number}</li>
+// );
+
+// ReactDOM.render (
+//     <ul>{listItems}</ul>,
+//     document.getElementById('root')
+// );
+
+// function NumberList (props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map ((number) => 
+//         <li key={number.toString()}>{number}</li>
+//     );
+//     return (
+//         <ul>{listItems}</ul>
+//     );
+// }
+// const numbers = [1,2,3,4,5];
+// ReactDOM.render(
+//     <NumberList numbers = {numbers} />,
+//     document.getElementById('root')
+// );
+
+function ListItem ( props ) {
+    return <li>{props.value}</li> ;
+}
+
+function NumberList (props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map ((number) => 
+        <ListItem key={number.toString()} value={number}/>
+    );
     return (
-        <div className="warning">
-            Warning!
-        </div>
+        <ul>{listItems}</ul>
     );
 }
-
-class Page extends React.Component {
-
-    constructor (props) {
-        super (props);
-        this.state = {showWarning : true};
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick () {
-        this.setState (state => ({
-            showWarning : !state.showWarning
-        }));
-    }
-
-    render () {
-        return ( 
-            <div>
-                <WarningBanner warn = {this.state.showWarning} />
-                <button onClick = {this.handleToggleClick}>
-                    {this.state.showWarning ? 'hide' : 'show'}
-                </button>
-            </div>
-        );
-    }
-}
-ReactDOM.render (
-    <Page/>,
+const numbers = [1,2,3,4,5];
+ReactDOM.render(
+    <NumberList numbers = {numbers} />,
     document.getElementById('root')
 );

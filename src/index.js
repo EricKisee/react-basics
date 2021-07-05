7b92597c22d1a7dc9bcf3fe05d73b3f0e535b519
Eric Kisee <erickiseemulwa@gmail.com>
2021-07-05 19:24:47 +0300

    Composition vs Inheritance - Containment with holes

--- src/index.js
+++ src/index.js
@@ -654,28 +654,50 @@ import './index.css';
 
 // -----------containment--------------
 
-function FancyBorder (props) {
+// function FancyBorder (props) {
+//     return (
+//         <div className= {'FancyBorder FancyBorder-' + props.color}>
+//             {props.children}
+//         </div>
+//     );
+// }
+
+// function WelcomeDialog () {
+//     return (
+//         <FancyBorder color="blue">
+//             <h1 className="Dialog-title">
+//                 Welcome
+//             </h1>
+//             <p className="Dialog-message">
+//                 Thanks for visiting our spacecraft
+//             </p>
+//         </FancyBorder>
+//     );
+// }
+
+// ReactDOM.render(
+//     <WelcomeDialog/>,
+//     document.getElementById('root')
+// );
+
+function SplitPane (props) {
     return (
-        <div className= {'FancyBorder FancyBorder-' + props.color}>
-            {props.children}
+        <div className = "SplitPane">
+            <div className="SplitPane-left">
+                {props.left}
+            </div>
+            <div className="SplitPane-right">
+                {props.right}
+            </div>
         </div>
     );
 }
 
-function WelcomeDialog () {
+function App () {
     return (
-        <FancyBorder color="blue">
-            <h1 className="Dialog-title">
-                Welcome
-            </h1>
-            <p className="Dialog-message">
-                Thanks for visiting our spacecraft
-            </p>
-        </FancyBorder>
+        <SplitPane 
+            left={<Contacts />} 
+            right={<Chat />} 
+        />
     );
 }
-
-ReactDOM.render(
-    <WelcomeDialog/>,
-    document.getElementById('root')
-);
